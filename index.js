6f50fdc2bb47b19f0f01a72b9772e6fe0413a6d5
Ommp <dallande@protonmail.com>
2023-10-03 23:36:42 +0200

    Use node/npm instead of bun

--- index.js
+++ index.js
@@ -1,45 +1,40 @@
 const http = require('http');
-const https = require('https');
+
+// import { Client } from 'pg';
+// const client = new Client()
+// await client.connect()
+
+const PORT = 3000;
+
+
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/views'));
 
 
 
-const Koa = require('koa');
-const app = new Koa();
-const Router = require('@koa/router');
-const { bodyParser } = require("@koa/bodyparser");
-const views = require('koa-views');
-const render = views(__dirname + '/views', { extension: 'nunjucks' });
+app.get('/', (req, res) => {
+  res.render('index', {title: "Hjem"});
+})
+app.get('/prosjekter', (req, res) => {
+  res.render('prosjekter', {title: "Prosjekter"});
+})
+app.get('/webutvikling', (req, res) => {
+  res.render('webutvikling', {title: "Webutvikling"});
+})
 
 
-const HTTP_PORT = 3002;
-const HTTPS_PORT = 3003;
-
-const router = new Router();
-
-app.use(render);
-
-router.get('/', (ctx, next) => {
-//   ctx.body = "Hjem";
-    console.log(__dirname + '/views/index.html');
-    ctx.render(__dirname + '/views/index.html');
-  
-});
-router.get('/webutvikling', (ctx, next) => {
-  ctx.body = "Webutvikling";
-});
-router.get('/prosjekter', (ctx, next) => {
-  ctx.body = "Prosjekter";
-});
+app.listen(PORT, () => {
+  console.log(`Running http on port localhost:${PORT}`);
+})
 
 
-app
-.use(router.routes())
-.use(router.allowedMethods)
-.use(bodyParser);   
 
-
-// app.listen(PORT);
-http.createServer(app.callback()).listen(HTTP_PORT);
-https.createServer(app.callback()).listen(HTTPS_PORT);
-
-console.log(`Running http on port ${HTTP_PORT} and https on port ${HTTPS_PORT}`);
