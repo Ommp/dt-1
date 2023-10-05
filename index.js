dc6cec10172fbb122edb2338eca12f868898f2a6
Ommp <dallande@protonmail.com>
2023-10-05 18:35:43 +0200

    Add number guessing game

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const path = require('path');
 
 // import { Client } from 'pg';
 // const client = new Client()
@@ -12,30 +13,32 @@ const ejs = require('ejs');
 
 const app = express();
 
-
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
 app.use(express.static(__dirname + '/css'));
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname + '/scripts'));
 
 
 
 app.get('/', (req, res) => {
   res.render('index', {title: "Hjem"});
-})
+});
 app.get('/prosjekter', (req, res) => {
   res.render('prosjekter', {title: "Prosjekter"});
-})
+});
+app.get('/prosjekter/:id', (req, res) => {
+  res.render('prosjekter/' + req.params.id, {title: req.params.id});
+});
 app.get('/webutvikling', (req, res) => {
   res.render('webutvikling', {title: "Webutvikling"});
-})
+});
+
 
 
 app.listen(PORT, () => {
   console.log(`Running http on port localhost:${PORT}`);
 })
-
-
-
---
+++ scripts/guessthenumber.js
@@ -0,0 +1,41 @@
+const start_button = document.getElementById("start");
+const submit_guess = document.getElementById("submit-guess");
+
+const guess_result = document.getElementById("guess-result");
+
+let actual_number;
+
+let guesses = [];
+
+function startGame() {
+
+    let min = parseInt(document.getElementById("min").value);
+    let max = parseInt(document.getElementById("max").value);
+    actual_number = Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function processGuess() {
+    let guess = parseInt(document.getElementById("guessed_number").value);
+    guesses.push(guess);
+
+    if (guess > actual_number) {
+        guess_result.innerHTML = "Too high!";
+    } else if (guess < actual_number) {
+         guess_result.innerHTML = "Too low!";
+    } else if (guess === actual_number) {
+        guess_result.innerHTML = "Correct!";
+    }
+
+    const li_node = document.createElement("li");
+    const guess_node = document.createTextNode(guess);
+    li_node.appendChild(guess_node);
+    document.getElementById("guesses").appendChild(li_node);
+}
+
+submit_guess.addEventListener("click", function() {
+    processGuess();
+})
+
+start_button.addEventListener("click", function() {
+    startGame();
+});
